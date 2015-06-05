7c782147f8775e85b15bc17986c9a3e2ea25cde8
Sam Rogers <jasarogers@gmail.com>
2015-06-05 16:03:06 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -2,7 +2,6 @@
 """
 Created on Fri Jun 05 12:55:50 2015
 
-@author: roge2582
 """
 
 # BGS Borehole URL Format http://scans.bgs.ac.uk/sobi_scans/boreholes/xxxxxx
@@ -12,15 +11,17 @@ import csv
 import requests
 
 # Setting up csv file to work into
-#f = csv.writer(open("bgsBoreholes.csv", "w"))
-#f.writerow(["BGS ID", "BGS Reference"])
+f = csv.writer(open("bgsBoreholes.csv", "w"))
+f.writerow(["BGS ID", "BGS Reference"])
 
 #for i in xrange(1,10):
-for i in xrange(0,1):
-    #Link = "http://scans.bgs.ac.uk/sobi_scans/boreholes/%d" %i    
-    Link = 'http://scans.bgs.ac.uk/sobi_scans/boreholes/13207806'   
+for i in xrange(0,10000):
+    Link = "http://scans.bgs.ac.uk/sobi_scans/boreholes/%d" %i    
+    #Link = 'http://scans.bgs.ac.uk/sobi_scans/boreholes/13207806'   
     r = requests.get(Link, timeout = 1)
-    boreholeReference = BeautifulSoup(r.text)
-    title = boreholeReference.title.text
-    #f.writerow(i , title)
-    print title
+    if r != "<Response [404]>": 
+        boreholeReference = BeautifulSoup(r.text)
+        title = boreholeReference.title.text
+        print title
+        f.writerow([i, title])
+    
