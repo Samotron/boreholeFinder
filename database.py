1577ed7029aa6d237aa01dd184356c7cec865348
Sam Rogers <james.s.rogers23@gmail.com>
2015-06-30 14:07:38 +0100

    Added init

--- database.py
+++ database.py
@@ -1,7 +1,9 @@
 __author__ = 'roge2582'
 
 import sqlite3
+from scraper import bgs_scraper
 
 def database():
     conn = sqlite3.connect(r"~\Data\aaa.db")
     cur = conn.cursor()
+    bgs_scraper()
